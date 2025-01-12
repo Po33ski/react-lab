aba02ff193c5dd8cb304af0fe832da5335ea973c
Student <student@d17-327e-pc-08.ipa.ii.agh.edu.pl>
2025-01-12 12:12:52 +0100

    dekompozycja

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import "milligram";
 import {useState} from "react";
-
+import MovieForm from "./MovieForm";
 
 
 function App() {
-  const [title, setTitle] = useState('Wall-E');
-  const [year, setYear] = useState(1994);
   const [movies, setMovies] = useState([ 
     {title: "Wall-E",
       year: 2000
@@ -22,44 +20,17 @@ function App() {
   ])
 
 
-  function handleTitle(event) {
-    setTitle(event.target.value);
-  }
-  function handleYear(event) {
-    setYear(event.target.value);
-  }
-
-  // function addMovie(){
-  //   setMovies([...movies, {title: title}]);
-  // }
-
-  let message;
-  if (title.length < 5) {
-    message = <div>Tytul jest bardzo krotki</div>
-  } 
-  else if (title.lenght < 15){
-    message = <div>Tytuł jest ekstra, w sam raz na plakat przed kinem!</div>;
-  } else {
-    message = <div>Tytuł jest za długi, nikt tego nie zapamięta.</div>;
-  }
-
   return (
-      <div>
+    <div>
         <h1>My favourite movies to watch</h1>
-        <div>
+     <div>
         <h1>Titles</h1>
         <ul>
-            {movies.map((movie) => <li key={movie.title}>{movie.title}</li>)}
+          {movies.map((movie) => <li key={movie.title}>{movie.title}</li>)}
         </ul>
-        </div>
-          <h2>Add Movie</h2>
-            <h3>Titel</h3>
-            <input type="text" value={title} onChange={handleTitle}/>
-            {title.length > 0 && <div>{message}</div>}
-            <h3>Year</h3>
-            <input type="number" value={year} onChange={handleYear}/>
-            <button type="button" onClick={() => {setMovies([...movies, {title: title, year: year}])}}>Dodaj</button>
-      </div>
+    </div>
+        <MovieForm onMovieSubmit={(movie) => setMovies([...movies, movie])}/>
+    </div>
   );
 }
 export default App;
