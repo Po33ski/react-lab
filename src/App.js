9d3933380038c64069797276e8b10055aa66b314
Student <student@d17-327e-pc-08.ipa.ii.agh.edu.pl>
2025-01-12 11:22:37 +0100

    lista filmow

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import './App.css';
 import {useState} from "react";
 
+const movies = [
+
+  {title: "Wall-E"},
+
+  {title: "Pulp Fiction"},
+
+  {title: "Matrix"},
+
+  {title: "1670"},
+
+];
 function App() {
   const [title, setTitle] = useState('Wall-E');
   function handleChange(event) {
@@ -18,6 +29,11 @@ else if (title.lenght < 15){
 }
   return (
       <div>
+        <div>
+        <ul>
+            {movies.map((movie) => <li key={movie.title}>{movie.title}</li>)}
+        </ul>
+        </div>
           <h1>My favourite movies to watch</h1>
           <h2>My favourite movie for today is {title}</h2>
             {title.length > 0 && <div>{message}</div>}
