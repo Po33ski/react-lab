25e1a6c5d7544eb3f56882836e1bf5e77825340a
Student <student@d17-327e-pc-08.ipa.ii.agh.edu.pl>
2025-01-12 10:37:12 +0100

    dodanie przycisku

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import {useState} from "react";
 function App() {
   const [title, setTitle] = useState('Wall-E');
   function handleChange(event) {
-
     setTitle(event.target.value);
-
 }
+
   return (
       <div>
           <h1>My favourite movies to watch</h1>
           <h2>My favourite movie for today is {title}</h2>
+          <div>
           <input type="text" value={title} onChange={handleChange}/>
+          <button type="button" onClick={() => alert(title)}>Pokaz Tytul filmu</button>
+          </div>
       </div>
   );
 }
