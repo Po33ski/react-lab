ea13504b8a0dc4816fe9ac730084f95fddd50d57
Student <student@d17-327e-pc-08.ipa.ii.agh.edu.pl>
2025-01-12 10:24:10 +0100

    reaktywnosc

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import './App.css';
+import {useState} from "react";
 
 function App() {
-
-  let title = 'Wall-E';
+  const [title, setTitle] = useState('Wall-E');
   function handleChange(event) {
-      console.log(event.target.value);
-  }
+
+    setTitle(event.target.value);
+
+}
   return (
       <div>
           <h1>My favourite movies to watch</h1>
           <h2>My favourite movie for today is {title}</h2>
-          <input type="text" onChange={handleChange}/>
+          <input type="text" value={title} onChange={handleChange}/>
       </div>
   );
-
 }
 export default App;
