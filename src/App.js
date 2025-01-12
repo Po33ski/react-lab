80643959672611bf9ce95b6e1bda4ed9ce8c9689
Student <student@d17-327e-pc-08.ipa.ii.agh.edu.pl>
2025-01-12 10:48:33 +0100

    dodatek

--- src/App.js
+++ src/App.js
@@ -7,13 +7,23 @@ function App() {
     setTitle(event.target.value);
 }
 
+let message;
+if (title.length < 5) {
+  message = <div>Tytul jest bardzo krotki</div>
+} 
+else if (title.lenght < 15){
+  message = <div>Tytuł jest ekstra, w sam raz na plakat przed kinem!</div>;
+} else {
+  message = <div>Tytuł jest za długi, nikt tego nie zapamięta.</div>;
+}
   return (
       <div>
           <h1>My favourite movies to watch</h1>
           <h2>My favourite movie for today is {title}</h2>
+            {title.length > 0 && <div>{message}</div>}
           <div>
-          <input type="text" value={title} onChange={handleChange}/>
-          <button type="button" onClick={() => alert(title)}>Pokaz Tytul filmu</button>
+            <input type="text" value={title} onChange={handleChange}/>
+            <button type="button" onClick={() => alert(title)}>Pokaz Tytul filmu</button>
           </div>
       </div>
   );
