ed281a207850132b580b45f1fb6111e3f153e68e
Student <student@d17-327e-pc-08.ipa.ii.agh.edu.pl>
2025-01-25 10:57:35 +0100

    add changes

--- src/Movie.js
+++ src/Movie.js
@@ -1,7 +1,11 @@
 export default function Movie (props) {
+
+    
     return (
         <div>
-            
+            <li key={props.movie.title}>{props.movie.title} {props.movie.year} 
+                <button onClick={props.deleteMovie(props.movie.title)}></button>
+            </li>
         </div>
     )
 }
--- src/MoviesList.js
+++ src/MoviesList.js
@@ -1,10 +1,16 @@
+import Movie from "./Movie"
+
 export default function MoviesList (props) {
+
+    function deleteMovie (title) {
+        props.
+    }
     return (
         <div>
             <h2>Movies List</h2>
             {props.movies.length < 1 ? <h2>The list of movies is empty</h2> :
         <ul>
-            {props.movies.map((movie) => <li key={movie.title}>{movie.title}</li>)}
+            {props.movies.map((movie) => <Movie movie={movie} deleteMovie={(title) => deleteMovie(title)}/>)}
         </ul>}
         </div>
     )
